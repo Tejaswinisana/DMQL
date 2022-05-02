576f923353b1621b173da7a07e205fade720fc19
Tejaswini Sana <tejaswinisana@Tejaswinis-MacBook-Air.local>
2022-05-02 02:44:37 -0400

    added readme to all the web files needed for web application

---
+++ README.md
@@ -0,0 +1,19 @@
+# Installation:
+## step 1: 
+> pip install flask
+> pip install psycopg2
+## step 2: 
+cd DMQL_project folder
+## step3: 
+On the command line 
+> flask run 
+#### Note: Server runs on localhost 5000 port if you want to exit use ctrl+c 
+
+### Make sure to change the db password and user name according to your local postgresql passwords. ( edit app.py following four variables )
+
+DB_HOST = "localhost"
+DB_NAME = "Shoppingcart"
+DB_USER = "postgres"
+DB_PASS = "1234"
+
+
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+import psycopg2
+import psycopg2.extras
+
+app= Flask(__name__)
+app.secret_key = "cairocoders-ednalan"
+
+DB_HOST = "localhost"
+DB_NAME = "Shoppingcart"
+DB_USER = "postgres"
+DB_PASS = "1234"
+
+conn = psycopg2.connect(dbname= DB_NAME, user = DB_USER,password =DB_PASS, host= DB_HOST)
+
+@app.route('/')
+def Index():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s= "select * from product"
+    cur.execute(s)
+    list_products = cur.fetchall()
+    return render_template('index.html',list_products=list_products)
+@app.route('/delete/<string:id>', methods = ['POST','GET'])
+def delete_student(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    print(cur.mogrify("DELETE FROM product WHERE prod_id = '{0}'".format(id)))
+    cur.execute("DELETE FROM product WHERE prod_id = '{0}'".format(id))
+    
+    conn.commit()
+    flash('Product Removed Successfully')
+    return Index()
+
+@app.route('/add_product', methods=['POST'])
+def add_student():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        prod_cat = request.form['prod_cat']
+        prod_sub_cat = request.form['prod_sub_cat']
+        prod_id = request.form['prod_id']
+        cur.execute("INSERT INTO product VALUES (%s,%s,%s)", (prod_cat, prod_sub_cat, prod_id))
+        conn.commit()
+        flash('Product Added successfully')
+        return Index()
